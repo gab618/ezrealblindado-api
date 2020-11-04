ce7dd8e60beb6d8ebcbc36a55f2606867703c3f7
Gabriel <knuckles4x@gmail.com>
2020-11-04 16:45:07 -0300

    games migrations

---
+++ src/database/migrations/20201104192942-create-logs.js
@@ -0,0 +1,31 @@
+module.exports = {
+  up: async (queryInterface, Sequelize) =>
+    queryInterface.createTable('logs', {
+      id: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+      },
+      user: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      points: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+      },
+      created_at: {
+        type: Sequelize.DATE,
+        allowNull: false,
+      },
+      updated_at: {
+        type: Sequelize.DATE,
+        allowNull: false,
+      },
+    }),
+
+  down: async (queryInterface, Sequelize) => {
+    return queryInterface.dropTable('logs');
+  },
+};
