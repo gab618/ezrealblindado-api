028f877813df0e7f668a856c2d4d3958a459fb50
Gabriel <knuckles4x@gmail.com>
2020-11-04 21:34:18 -0300

    game controller

---
+++ src/app/controllers/GameController.js
@@ -0,0 +1,36 @@
+const Game = require('../models/Game');
+const Log = require('../models/Log');
+const { nanoid } = require('nanoid');
+const { decryptWithAES } = require('../../utils/aes');
+const seApi = require('../../services/se');
+
+class GameController {
+  async store(req, res) {
+    const key = nanoid(12);
+    await Game.create({ key });
+    return res.json({ key });
+  }
+
+  async delete(req, res) {
+    const { key, user, hash } = req.body;
+
+    const points = decryptWithAES(hash);
+
+    const validKey = await Game.findOne({ where: { key } });
+    if (!validKey) {
+      return res.status(401).json({ message: 'Invalid submit' });
+    }
+    await validKey.destroy();
+
+    try {
+      await seApi.put(`${user}/${points}`);
+      await Log.create({ user, points });
+    } catch (error) {
+      return res.status(500).json({ error: 'Request error ' + error });
+    }
+
+    return res.json({ message: `Points assigned to ${user}` });
+  }
+}
+
+module.exports = new GameController();
