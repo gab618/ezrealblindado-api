3c3dcf34a1f46708b137105b0e682aa2adba0b72
Gabriel <knuckles4x@gmail.com>
2020-12-09 17:32:52 -0300

    create new header

--- src/app/controllers/HeaderController.js
+++ src/app/controllers/HeaderController.js
@@ -2,32 +2,41 @@ const Header = require('../models/Header');
 
 class HeaderController {
   async show(req, res) {
-    const header = await Header.findOne();
-    if (header) {
-      return res.send(header.text);
+    const headerList = await Header.findAll({ where: { is_completed: false } });
+
+    if (headerList.length === 0) return res.send('🤡🤡🤡');
+
+    if (headerList.length === 1) {
+      if (!headerList[0].start_time) {
+        console.log('criando start_time...');
+        await headerList[0].update({ start_time: new Date() });
+      }
+      await headerList[0].update({ is_started: true });
+      return res.send(`"${headerList[0].text}" - ${headerList[0].sender}`);
     }
-    return res.send('🤡🤡🤡🤡🤡🤡🤡');
+
+    return res.send('todo');
   }
 
   async update(req, res) {
     const secretPass = process.env.CREATE_URL_PASS;
-    const { msg, sender, pass } = req.query;
+    const { text, sender, pass } = req.query;
 
     if (!pass || pass !== secretPass) {
       return res.status(401).json({ error: 'Permission denied' });
     }
 
-    const text = `${msg} - ${sender}`;
+    const headerExists = await Header.findOne({
+      where: { is_completed: false, sender: sender },
+    });
 
-    const header = await Header.findOne();
-    if (!header) {
-      await Header.create({ text });
-      return res.send('ok');
+    if (headerExists) {
+      return res.send('Vc ja tem um header amigão');
     }
 
-    await header.update({ text });
+    await Header.create({ text, sender });
 
-    return res.json(header);
+    return res.json(`@${sender} header adicionado a fila`);
   }
 }
 
